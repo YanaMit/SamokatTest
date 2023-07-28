b0dadcdcd00235308a7bf7e6e88a85e6e97305ba
Yana Mitiushina <krasniipanda@yandex.ru>
2023-07-29 02:54:28 +0300

    second fix Samokat tests

--- SamokatTest/src/main/java/pom/OrderPage.java
+++ SamokatTest/src/main/java/pom/OrderPage.java
@@ -30,17 +30,15 @@ public class OrderPage {
     private static final By NEXT_BUTTON = By.xpath(".//div[@class='Order_NextButton__1_rCA']/button");
 
     //указание полей:
-    private static final By STATION = By.xpath(".//div[@class='Order_Text__2broi' and text()='Черкизовская']");
-
     private static final By DATE = By.xpath(".//input[@placeholder='* Когда привезти самокат']");
 
-    private static final By TODAY = By.xpath(".//div[@class='react-datepicker']//div[contains(@class, '--today')]");
+    private static final By CHOOSED_DAY = By.xpath(".//div[@class=\"react-datepicker\"]//div[contains(@class, \"--selected\")]");
 
     private static final By HOW_LONG = By.xpath(".//div[text()='* Срок аренды']");
 
-    private static final By ONE_DAY = By.xpath(".//div[@class='Dropdown-menu']//div[text()='сутки']");
+    private static final By COLOR_BLACK = By.xpath("//input[@id='black']");
 
-    private static final By COLOR = By.xpath("//input[@id='black']");
+    private static final By COLOR_GREY = By.xpath("//input[@id='grey']");
 
     private static final By ORDER_BUTTON = By.xpath("//div/button[@class='Button_Button__ra12g Button_Middle__1CSJM']");
 
@@ -69,46 +67,55 @@ public class OrderPage {
         driver.findElement(NEXT_BUTTON).click();
     }
 
-    //выбрать метро
-    public void clickMetroStation(){
-        driver.findElement(STATION).click();
+    //выбрать метро //редактировала
+    public void clickMetroStation(String metroStation){
+        By stationButton = By.xpath(".//div[@class='Order_Text__2broi' and text()='" + metroStation + "']");
+        isVisible(stationButton);
+        driver.findElement(stationButton).click();
     }
 
     //выбрать дату
-    public void pickDate() {
-        driver.findElement(DATE).click();
-        driver.findElement(TODAY).click();
+    public void pickDate(String date) {
+        driver.findElement(DATE).sendKeys(date);
+        driver.findElement(CHOOSED_DAY).click();
     }
 
+
     //выбрать, на какой срок
-    public void pickHowLong() {
+    public void pickHowLong(String howLong) {
         driver.findElement(HOW_LONG).click();
-        driver.findElement(ONE_DAY).click();
+        driver.findElement(By.xpath(".//div[@class='Dropdown-option' and text()='" + howLong +"']")).click();
     }
 
     //выбрать цвет
-    public void chooseColor(){
-        driver.findElement(COLOR).click();
+    public void chooseColor(String color){
+        if (color.equals("черный жемчуг")) {
+            driver.findElement(COLOR_BLACK).click();
+        }
+        else {driver.findElement(COLOR_GREY).click();
+        }
     }
 
     //шаг сделать заказ
-    public void makeOrder(String name, String lastName, String adress, String metroStation, String phoneNumber){
+    public void makeOrder(String name, String lastName, String adress, String metroStation,
+                          String phoneNumber, String date, String howLong, String color) {
 
         isVisible(NAME_FIELD);
         printInput(name, NAME_FIELD);
         printInput(lastName, LASTNAME_FIELD);
         printInput(adress, ADRESS_FIELD);
         printInput(metroStation, METRO_FIELD);
-        clickMetroStation();
+        clickMetroStation(metroStation);
         printInput(phoneNumber, PHONE_NUMBER_FIELD);
         clickNext();
-        pickDate();
-        pickHowLong();
-        chooseColor();
+        pickDate(date);
+        pickHowLong(howLong);
+        chooseColor(color);
         driver.findElement(ORDER_BUTTON).click();
         driver.findElement(YES_BUTTON).click();
         isVisible(MADE_ORDER_WINDOW);
     }
+
     public String getActualText() {
         String actualText = driver.findElement(MADE_ORDER).getText();
         return actualText;
--- SamokatTest/src/test/java/OrderTests.java
+++ SamokatTest/src/test/java/OrderTests.java
@@ -26,19 +26,30 @@ public class OrderTests extends SetupBrowser {
     private String adress;
     private String metroStation;
     private String phoneNumber;
-    public OrderTests(String name, String lastName, String adress, String metroStation, String phoneNumber) {
+    private String date;
+    private String howLong;
+    private String color;
+
+    public OrderTests(String name, String lastName, String adress, String metroStation,
+                      String phoneNumber, String date, String howLong, String color) {
         this.name = name;
         this.lastName = lastName;
         this.adress = adress;
         this.metroStation = metroStation;
         this.phoneNumber = phoneNumber;
+        this.date = date;
+        this.howLong = howLong;
+        this.color = color;
     }
 
     @Parameterized.Parameters
     public static Object[][] myOrderTests() {
         return new Object[][]{
 
-                {"Григорий", "Фунтиков", "г. Москваб ул. Ленинаб д. 1", "Черкизово", "+79051234567"},
+                {"Григорий", "Фунтиков", "г. Москва, ул. Ленина, д. 1", "Черкизовская",
+                        "+79051234567", "11.08.2023", "сутки", "черный жемчуг"},
+                {"Мария", "Иванова", "г. Саратов ул. Чушкина д. 123", "Павелецкая",
+                        "+79260234597", "17.08.2023", "трое суток", "серая безысходность"},
                };
     }
 
@@ -48,7 +59,7 @@ public class OrderTests extends SetupBrowser {
         MainPage mainPage = new MainPage(driver);
         mainPage.open();
         mainPage.clickVisibleFirstOrderButton();
-        orderPage.makeOrder(name, lastName, adress, metroStation, phoneNumber);
+        orderPage.makeOrder(name, lastName, adress, metroStation, phoneNumber, date, howLong, color);
         Assert.assertTrue(orderPage.getActualText().contains("Заказ оформлен"));
     }
 
@@ -58,7 +69,7 @@ public class OrderTests extends SetupBrowser {
         MainPage mainPage = new MainPage(driver);
         mainPage.open();
         mainPage.scrollAndClickSecondOrderButton();
-        orderPage.makeOrder(name, lastName, adress, metroStation, phoneNumber);
+        orderPage.makeOrder(name, lastName, adress, metroStation, phoneNumber, date, howLong, color);
         Assert.assertTrue(orderPage.getActualText().contains("Заказ оформлен"));
     }
 
